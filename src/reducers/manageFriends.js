8f42c8c16b657e8a6f540fb59c9caf2ea8f027ea
Matt <mattfsewell@gmail.com>
2021-04-01 12:06:11 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+let state;
+export function manageFriends(state = { friends: [] }, action){
+        
+        switch (action.type) {
+            case 'ADD_FRIEND':
+                return {friends: state.friends.push(action.friend)}
+            case 'REMOVE_FRIEND':
+                return {}
+            default:
+                return state;
+        }
+    
 }
